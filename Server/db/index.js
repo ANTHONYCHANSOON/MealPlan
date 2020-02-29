23343d2e0b0a90275d3fadde850fc8b74ab4ca22
anthony soon <anthonychansoon@gmail.com>
2020-02-29 03:30:03 -0800

    get and get specific

--- Server/db/index.js
+++ Server/db/index.js
@@ -4,23 +4,65 @@ var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
+    connection = mysql.createPool({
+        connectionLimit: 10,
         host: "localhost",
         user: "root",
         password: "!Anthon69",
-        database: "ourmealplan"
+        database: "ourmealplan",
+        port: '3306'
+    });
+}
+
+let mealdb = {};
+
+mealdb.all = () => {
+    return new Promise((resolve, reject) => {
+        connection.query("select * from meal", (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        })
     })
 }
 
-connection.connect(function(err) {
-    if(err) {
-        console.error("error connecting " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-})
+mealdb.one = (id) => {
+    return new Promise((resolve, reject) => {
+        connection.query("select * from meal where id = ?", [id], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results[0]);
+        })
+    })
+}
 
-module.exports = connection;
+module.exports = mealdb;
+
+// const mysql = require('mysql');
+// var connection;
+
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         user: "root",
+//         password: "!Anthon69",
+//         database: "ourmealplan"
+//     })
+// }
+
+// connection.connect(function(err) {
+//     if(err) {
+//         console.error("error connecting " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// })
+
+// module.exports = connection;
 
 // let mealinfo = {};
 
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,30 +1,58 @@
-// var express = require("express");
-// var router = express.Router();
+const express = require("express");
+const db = require('../db');
+const router = express.Router();
 
-// router.get("/", function (req, res) => {
+router.get('/', async (req, res, next) => {
+    try {
+        let results = await db.all();
+        res.json(results);
+    }
+    catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        let results = await db.one(req.params.id);
+        res.json(results);
+    }
+    catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+module.exports = router
+
+// // var express = require("express");
+// // var router = express.Router();
+
+// // router.get("/", function (req, res) => {
+
+// // })
+
+
+
+
+// const express = require("express");
+// const router = express.Router();
+// const db = require("../db")
+
+// router.get("/", function(req,res) {
 
 // })
 
+// // router.get('/', async (req, res, next) => {
+// //     try {
+// //         let results = await db.all();
+// //         res.json(results);
+// //     }
+// //     catch(e) {
+// //         console.log(e);
+// //         res.sendStatus(500);
+// //     }
+// // });
 
-
-
-const express = require("express");
-const router = express.Router();
-const db = require("../db")
-
-router.get("/", function(req,res) {
-    
-})
-
-// router.get('/', async (req, res, next) => {
-//     try {
-//         let results = await db.all();
-//         res.json(results);
-//     }
-//     catch(e) {
-//         console.log(e);
-//         res.sendStatus(500);
-//     }
-// });
-
-module.exports = router;
+// module.exports = router;
